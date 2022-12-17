66049cac9249906b4eca6634c3fb725a0ad58e98
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-18 05:13:33 +0900

    chore :: security 변경

--- src/main/java/com/example/appjamserver/global/security/SecurityConfig.java
+++ src/main/java/com/example/appjamserver/global/security/SecurityConfig.java
@@ -6,11 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.web.cors.CorsUtils;
 
 @RequiredArgsConstructor
@@ -53,4 +56,13 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public HttpFirewall httpFirewall() {
+        return new DefaultHttpFirewall();
+    }
+
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.httpFirewall(httpFirewall());
+    }
+    
 }
